cb8675a6608f5af3e13aecc261aed19a47a802a1
Dan Jutan <danjutan@gmail.com>
2021-06-18 16:25:22 -0400

    header center

--- components/Heading.js
+++ components/Heading.js
@@ -1,7 +1,11 @@
 
-export default function Heading({size, children}) {
+export default function Heading({size=1, children, centered=false}) {
   const fontSize = size == 1 ? 'xl' : size + 'xl';
-  return (<h1 className={`font-bold text-${fontSize} py-2`}>
+  let className =`font-bold text-${fontSize} py-2 `;
+  if (centered) {
+    className += "text-center"
+  }
+  return (<h1 className={className}>
     {children}
   </h1>)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Heading from "../components/Heading"
+import Image from "next/image"
+import mainPic from "../public/shaun_dan.jpg"
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
 
       <div
         className="w-11/12 md:w-2/3 xl:w-1/2 rounded-lg border shadow-lg p-10">
-        <Heading size={2}>Welcome to the Talmud Lab</Heading>
+        <Heading size={2} centered={true}>Welcome to the Talmud Lab</Heading>
         <p>
-          We're a small team working on research in digital humanities, data analysis, and user experience
+          We're a small team working on open-source research in digital humanities, data analysis, and UX
           with the goal of creating the best possible digital talmud experience.
         </p>
       </div>
